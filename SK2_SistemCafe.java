42dc660e594d1f84d64e3df90d73c0dcc2a1f3b9
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2024-12-13 18:45:11 +0700

    fungsi main benar

--- SK2_SistemCafe.java
+++ SK2_SistemCafe.java
@@ -3,7 +3,7 @@ public class SK2_SistemCafe {
 
     static Scanner sc=new Scanner(System.in);
     static String[] MenuCafe = {"KopiHitam", "Latte","TehTarik", "MiGoreng"};
-    static  int  [] HargaMenu = {"15000", "22000" , "12000" , "18000"};
+    static  int  [] HargaMenu = {15000, 22000 , 12000 , 18000};
     public static void main(String[] args) {
     int pilihan_menu ;
     for (pilihan_menu= 0 ; pilihan_menu != 3;pilihan_menu++ ){
@@ -19,8 +19,10 @@ public class SK2_SistemCafe {
 
         if (pilihan_menu == 1) {
             tambahpesanan();
+            break;
         } else if (pilihan_menu == 2 ) {
             tampilkanPesanan ();
+            break;
         } else if (pilihan_menu = 3) {
             System.out.println("Terima Kasih ! Program telah selesai.");
             break;
