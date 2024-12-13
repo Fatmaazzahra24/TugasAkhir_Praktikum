d9263c365a9b66d8a197ac8f30632b886c3f2d6b
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2024-12-13 18:42:52 +0700

    coba main

---
+++ SK2_SistemCafe.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class SK2_SistemCafe {
+
+    static Scanner sc=new Scanner(System.in);
+    static String[] MenuCafe = {"KopiHitam", "Latte","TehTarik", "MiGoreng"};
+    static  int  [] HargaMenu = {"15000", "22000" , "12000" , "18000"};
+    public static void main(String[] args) {
+    int pilihan_menu ;
+    for (pilihan_menu= 0 ; pilihan_menu != 3;pilihan_menu++ ){
+        System.out.println("========================================");
+        System.out.println("Selamat Datang di Sistem Pemesanan Cafe!");
+        System.out.println("========================================");
+        System.out.println("1. Tambahkan Pesanan ");
+        System.out.println("2. Tampilkan Daftar Pesanan ");
+        System.out.println("3. Keluar  ");
+        System.out.print(" Pilih menu yang tersedia : ");
+        pilihan_menu = sc.nextInt();
+        sc.nextLine();
+
+        if (pilihan_menu == 1) {
+            tambahpesanan();
+        } else if (pilihan_menu == 2 ) {
+            tampilkanPesanan ();
+        } else if (pilihan_menu = 3) {
+            System.out.println("Terima Kasih ! Program telah selesai.");
+            break;
+        } else {
+            System.out.println("Pilihan menu tidak tersedia! silahkan coba lagi.");
+        }
+    }
+    }
+    
+}
